2258bf241b3a6f3b93cd449120327abd47eed408
Захаров Павел <pzakharov83@gmail.com>
2024-02-04 13:09:14 +0300

    add state storage (search query)

--- app/src/main/java/com/example/android/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/android/playlistmaker/SearchActivity.kt
@@ -1,6 +1,7 @@
 package com.example.android.playlistmaker
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
@@ -10,13 +11,25 @@ import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var searchQuery: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        searchQuery = ""
+
+        if (savedInstanceState != null) {
+            searchQuery = savedInstanceState.getString(SEARCH_QUERY, "")
+        }
+
+
         val linearLayout = findViewById<LinearLayout>(R.id.container)
         val inputEditText = findViewById<EditText>(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        inputEditText.setText(searchQuery)
+
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
@@ -28,6 +41,7 @@ class SearchActivity : AppCompatActivity() {
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 clearButton.visibility = clearButtonVisibility(s)
+
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -37,11 +51,20 @@ class SearchActivity : AppCompatActivity() {
         inputEditText.addTextChangedListener(simpleTextWatcher)
     }
 
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString(SEARCH_QUERY, searchQuery)
+    }
+
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
             View.GONE
         } else {
             View.VISIBLE
         }
+    }
+
+    companion object {
+        const val SEARCH_QUERY = "SEARCH_QUERY"
     }
 }
