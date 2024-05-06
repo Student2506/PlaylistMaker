a66013e3e142d7ed944063f702bb8d9583d6e9b6
Захаров Павел <pzakharov83@gmail.com>
2024-05-06 22:42:10 +0300

    move glide back to presentation layer

--- app/src/main/java/com/example/android/playlistmaker/Creator.kt
+++ app/src/main/java/com/example/android/playlistmaker/Creator.kt
@@ -1,15 +1,11 @@
 package com.example.android.playlistmaker
 
-import com.example.android.playlistmaker.data.TrackImageRepositoryImpl
 import com.example.android.playlistmaker.data.TracksRepositoryImpl
 import com.example.android.playlistmaker.data.network.RetrofitNetworkClient
 import com.example.android.playlistmaker.domain.api.AudioPlayerInteractor
-import com.example.android.playlistmaker.domain.api.TrackImageInteractor
-import com.example.android.playlistmaker.domain.api.TrackImageRepository
 import com.example.android.playlistmaker.domain.api.TracksInteractor
 import com.example.android.playlistmaker.domain.api.TracksRepository
 import com.example.android.playlistmaker.domain.impl.AudioPlayerInteractorImpl
-import com.example.android.playlistmaker.domain.impl.TrackImageInteractorImpl
 import com.example.android.playlistmaker.domain.impl.TracksInteractorImpl
 
 object Creator {
@@ -28,13 +24,5 @@ object Creator {
 
     fun provideAudioPlayer(): AudioPlayerInteractor {
         return audioPlayer
-    }
-
-    private fun getTrackImageRepository(): TrackImageRepository {
-        return TrackImageRepositoryImpl()
-    }
-
-    fun provideTrackImageInteracor(): TrackImageInteractor {
-        return TrackImageInteractorImpl(getTrackImageRepository())
     }
 }
--- app/src/main/java/com/example/android/playlistmaker/presentation/ui/audioplayer/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/android/playlistmaker/presentation/ui/audioplayer/AudioPlayerActivity.kt
@@ -5,12 +5,15 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.util.TypedValue
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.android.playlistmaker.Creator
 import com.example.android.playlistmaker.R
 import com.example.android.playlistmaker.data.dto.TrackDto
@@ -56,7 +59,6 @@ class AudioPlayerActivity : AppCompatActivity() {
         }
     }
     private val audioPlayer = Creator.provideAudioPlayer()
-    private val trackImageInteractor = Creator.provideTrackImageInteracor()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,7 +70,20 @@ class AudioPlayerActivity : AppCompatActivity() {
         }
         val track: TrackDto = intent.getParcelableExtra(TRACK_TO_SHOW)!!
         Log.d("AudioPlayer", track.toString())
-        trackImageInteractor.searchImage(track.artworkUrl100, trackImage)
+        Glide.with(applicationContext)
+            .load(track.artworkUrl100.replaceAfterLast('/', "512x512bb.jpg"))
+            .placeholder(R.drawable.placeholder)
+            .centerInside()
+            .transform(
+                RoundedCorners(
+                    TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP,
+                        ROUND_CORNERS_SIZE_PX,
+                        applicationContext.resources.displayMetrics
+                    ).toInt()
+                )
+            )
+            .into(trackImage)
         trackName.text = track.trackName
         artistName.text = track.artistName
         trackDurationTime.text = timeFormatter.format(track.trackTime)
