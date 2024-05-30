7d8dfe4017407731dde6af3d565a2cebfe71cf10
Захаров Павел <pzakharov83@gmail.com>
2024-05-30 11:22:22 +0300

    settings view binding

--- app/src/main/java/com/example/android/playlistmaker/settings/ui/SettingsActivity.kt
+++ app/src/main/java/com/example/android/playlistmaker/settings/ui/SettingsActivity.kt
@@ -3,24 +3,23 @@ package com.example.android.playlistmaker.settings.ui
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.android.playlistmaker.R
+import com.example.android.playlistmaker.databinding.ActivitySettingsBinding
 import com.example.android.playlistmaker.main.CustomApp
 import com.example.android.playlistmaker.settings.presentation.SettingsViewModel
-import com.google.android.material.switchmaterial.SwitchMaterial
 
 
 class SettingsActivity : ComponentActivity() {
 
     private var settingsViewModel: SettingsViewModel? = null
 
-    private var header: FrameLayout? = null
-    private var licenseAgreement: FrameLayout? = null
-    private var techSupport: FrameLayout? = null
-    private var shareApp: FrameLayout? = null
-    private var themeSwitcher: SwitchMaterial? = null
+    private val binding: ActivitySettingsBinding by lazy {
+        ActivitySettingsBinding.inflate(
+            layoutInflater
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,24 +27,17 @@ class SettingsActivity : ComponentActivity() {
             this,
             SettingsViewModel.getViewModelFactory()
         )[SettingsViewModel::class.java]
-        setContentView(R.layout.activity_settings)
-        header = findViewById<FrameLayout>(R.id.flBackToMainSettings)
-        licenseAgreement = findViewById<FrameLayout>(R.id.flShowAgreement)
-        techSupport = findViewById<FrameLayout>(R.id.flSendToTechsupport)
-        shareApp = findViewById<FrameLayout>(R.id.flShareButton)
-        themeSwitcher = findViewById<SwitchMaterial>(R.id.swNightMode)
-        header?.setOnClickListener {
+
+        setContentView(binding.root)
+        binding.flBackToMainSettings.setOnClickListener {
             finish()
         }
-
-
-        licenseAgreement?.setOnClickListener {
+        binding.flShowAgreement.setOnClickListener {
             val showAgreement = Intent(Intent.ACTION_VIEW)
             showAgreement.data = Uri.parse(getString(R.string.license_agreement_url))
             startActivity(showAgreement)
         }
-
-        techSupport?.setOnClickListener {
+        binding.flSendToTechsupport.setOnClickListener {
             val techSupportIntent = Intent(Intent.ACTION_SENDTO)
             techSupportIntent.data = Uri.parse("mailto:")
             techSupportIntent.putExtra(
@@ -62,19 +54,17 @@ class SettingsActivity : ComponentActivity() {
             )
             startActivity(techSupportIntent)
         }
-        shareApp?.setOnClickListener {
+        binding.flShareButton.setOnClickListener {
             val shareAppIntent = Intent(Intent.ACTION_SEND)
             shareAppIntent.setType("text/plain")
             shareAppIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.practicum_link))
             startActivity(shareAppIntent)
         }
-
-
-        themeSwitcher?.setOnCheckedChangeListener { _, checked ->
+        binding.swNightMode.setOnCheckedChangeListener { _, checked ->
             (applicationContext as CustomApp).switchTheme(checked)
             settingsViewModel?.themeSwitcher(checked)
         }
-        themeSwitcher?.isChecked = (applicationContext as CustomApp).darkTheme
+        binding.swNightMode.isChecked = (applicationContext as CustomApp).darkTheme
 
     }
 
