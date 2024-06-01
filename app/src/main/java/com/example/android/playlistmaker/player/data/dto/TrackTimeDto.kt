0d539b981f33d25bf0e19e8435cb400919337acd
Захаров Павел <pzakharov83@gmail.com>
2024-06-01 13:17:39 +0300

    remove view from settings

--- app/src/main/java/com/example/android/playlistmaker/player/data/dto/TrackTimeDto.kt
+++
@@ -1,5 +0,0 @@
-package com.example.android.playlistmaker.player.data.dto
-
-interface TrackTimeDto {
-    data class TrackTime(val time: Long) : TrackTimeDto
-}
--- app/src/main/java/com/example/android/playlistmaker/settings/presentation/SettingsView.kt
+++
@@ -1,4 +0,0 @@
-package com.example.android.playlistmaker.settings.presentation
-
-interface SettingsView {
-}
