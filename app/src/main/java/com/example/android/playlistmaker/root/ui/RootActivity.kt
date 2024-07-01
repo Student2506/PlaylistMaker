2d2d63382731434898b065a10eae95e1e1a2c996
Захаров Павел <pzakharov83@gmail.com>
2024-07-01 23:17:52 +0300

    bottom menu

--- app/src/main/java/com/example/android/playlistmaker/root/ui/RootActivity.kt
+++ app/src/main/java/com/example/android/playlistmaker/root/ui/RootActivity.kt
@@ -3,6 +3,9 @@ package com.example.android.playlistmaker.root.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.android.playlistmaker.R
 import com.example.android.playlistmaker.databinding.ActivityRootBinding
 import com.example.android.playlistmaker.medialibrary.ui.MediaLibraryFragment
@@ -17,10 +20,15 @@ class RootActivity : AppCompatActivity() {
 
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding?.root)
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                this.add(R.id.rootFragmentContainerView, MediaLibraryFragment())
-            }
-        }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding?.bottomNavigationView?.setupWithNavController(navController)
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.commit {
+//                this.add(R.id.rootFragmentContainerView, MediaLibraryFragment())
+//            }
+//        }
     }
 }
